07b41412aa61a7973ff1d22b7974e3742c79202e
Maruf68 <maruf.akib73@gmail.com>
2022-05-27 12:58:10 +0600

    database connection successfull

---
+++ user.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container my-5" >
+        <form method="post">
+            <div class="mb-3">
+                <label>Name</label>
+                <input type="text" class="form-control" autocomplete="off"   placeholder="Enter your username" name="name">
+            </div>
+
+
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control" autocomplete="off"    placeholder="Enter your email" name="email">
+            </div>
+
+            <div class="mb-3">
+                <label>Mobile</label>
+                <input type="email" class="form-control" autocomplete="off"    placeholder="Enter your mobile number" name="mobile">
+            </div>
+
+            <div class="mb-3">
+                <label>Password</label>
+                <input type="email" class="form-control" autocomplete="off"    placeholder="Enter your password password" name="password">
+            </div>
+
+
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>

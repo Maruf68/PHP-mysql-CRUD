4ba5940a20a48bd072f6603104ccac76c57f3cd1
Maruf68 <maruf.akib73@gmail.com>
2022-05-28 13:26:00 +0600

    new bootstrap login template

--- admin_login.php
+++ admin_login.php
@@ -18,22 +18,52 @@ include 'connect.php';
 
 
 
-    <div class="container">
-    <form method="post" >
-      
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Username</label>
-          <input type="text" class="form-control" name="AdminName" id="exampleInputEmail1" required>
-          
+    <section class="vh-100">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 text-black">
+
+     
+
+        <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+          <form method="post" style="width: 23rem;">
+
+            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+
+            <div class="form-outline mb-4">
+              <input type="text" id="form2Example18" name="AdminName" class="form-control form-control-lg" />
+              <label class="form-label" for="form2Example18">Username</label>
+            </div>
+
+            <div class="form-outline mb-4">
+              <input type="password" id="form2Example28" name="AdminPassword"  class="form-control form-control-lg" />
+              <label class="form-label" for="form2Example28">Password</label>
+            </div>
+
+            <div class="pt-1 mb-4">
+              <button class="btn btn-info btn-lg btn-block" name="signin" type="submit">Login</button>
+            </div>
+
+           
+
+          </form>
+
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" name="AdminPassword" id="exampleInputPassword1" required>
-        </div>
-       
-        <button type="submit" name="signin" class="btn btn-primary">Submit</button>
-      </form>
+
+      </div>
+      <div class="col-sm-6 px-0 d-none d-sm-block">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
+          alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+      </div>
     </div>
+  </div>
+</section>
+
+
+
+
+
 
 
 
@@ -49,7 +79,8 @@ include 'connect.php';
             header("location:display.php");
            }
            else{
-             echo   '<script>alert("Wrong username or password")</script>';
+           
+             echo '<script>alert("Wrong username or password")</script>';
            }
         }
 
